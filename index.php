18ae9afb18374a633806e9307bd68c98a8194a44
Richard <richard@voly.co.uk>
2024-03-19 11:36:49 +0000

    Echo spacing and indents to make clearer

--- index.php
+++ index.php
@@ -2,6 +2,7 @@
 
 $bottles = "";
 
+echo "\n";
 // For Loop starts from 99, decrease by 1 over every iteration.
 for ($counter = 99; $counter >= 1; $counter = $counter - 1) // If counter greater than or equal to 1 then decrease counter by one.
 {
@@ -10,14 +11,15 @@ for ($counter = 99; $counter >= 1; $counter = $counter - 1) // If counter greate
     } else {
         $bottles = 'bottles';
     }
-    echo $counter . " " . $bottles . " of beer on the wall. ";
+    echo $counter . " " . $bottles . " of beer on the wall.";
     if ($counter < 99) { // Need to repeat same this line again if counter below 99.
+        echo "\n\n";
         echo $counter . " " . $bottles . " of beer on the wall.";
     }
-    echo $counter . " " . $bottles . " of beer.";
-    echo "Take one down and Pass it around.";
+    echo " " . $counter . " " . $bottles . " of beer.";
+    echo " Take one down and Pass it around.";
     if ($counter == 1) {
-        echo "No bottles of beer on the wall.";
+        echo " No bottles of beer on the wall.";
     }
 }
 
